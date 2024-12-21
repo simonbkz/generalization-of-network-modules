a951e4cc32812eb6fd23ce66afe5c8e7e35a7976
simon <simonbkz@gmail.com>
2024-12-21 16:37:54 +0200

    adding some comments and implement dynamic formula to make sense of it and how entries are generated

--- sparse_network/sparse.py
+++ sparse_network/sparse.py
@@ -14,23 +14,24 @@ def svs(A, U, VT, num_svds, k2):
     # : VT --> right orthogonal matrix
     # : num_svds --> number of singular values needed to retain
     # : k2 --> not yet clear
-    S = np.dot(U.T, np.dot(A,VT.T)) #what is the purpose of this S
-    small_length = np.min([S.shape[0], S.shape[1]]) #
-    s = np.array([S[i,i] for i in range(small_length)])
+    S = np.dot(U.T, np.dot(A,VT.T)) #SDV
+    small_length = np.min([S.shape[0], S.shape[1]]) #lower-rank sub-structure, from S return fewest columns or rows and that is the lowest rank
+    s = np.array([S[i,i] for i in range(small_length)]) # construct a list of singular values, sorted from highest to lowest
     if k2 == 0:
         s = np.sort(s)[::-1] #only sorting eigenvalues if k2 == 0, otherwise not going to sort it
     s = s[:num_svds] #under what conditions are these not sorted, k2 == 1 and means will not be sorted
     return U, s, VT
 
-#TODO: test this and reference the literature if everything is code correctly
+#TODO: test this and reference the literature if everything is code correctly (svs function make sense)
 #TODO: add following component and ensure it works with the above 
 #TODO: how are we separating dense from sparse network
 
 def gen_binary_patterns(num_features):
     # Generates every unique binary digit possible with num_feature bits
     # do bits translates into (x,y) points (pixels)
+    # only applicable for compositional features, non compositional we take all features
     # TODO: explore in detail the num_features (does this refer to the number of entries in a matrix?)
-    data = np.ones((2**num_features, num_features))*-1.0 #why are we shifting this and what does it mean
+    data = np.ones((2**num_features, num_features))*-1.0 #generate data placeholder matrix
     for j in np.arange(0, num_features, 1):
         step = 2**(j+1)
         idx = [list(range(i,i+int(step/2))) for i in np.arange(int(step/2),2**num_features,step)]
@@ -40,13 +41,26 @@ def gen_binary_patterns(num_features):
     return data
 
 # TODO: we will need to unpack the dynamic_formular which generates the data that we need
-def dynamic_formular(n1,n2):
+def dynamic_formular(n1,n2, k1, k2, r, a_init, num_time_steps, num_svds):
     """
     return predictions of training dynamics of the linear network using formular for the Singular Vector Decomposition for the first generation of training
     sets up some constant matrices consistent with those used in the appendix of specialisation of network modules for the svd equations
     """
+    # nx - number of bits in compositional matrix, rows will be 2**nx
+    n1 = 3 #num sys inputs, compositional
+    n2 = 1 #num sys outputs, compositional
+    k1 = 3 #num non-sys inputs
+    k2 = 1 #num non-sys outputs
 
-    return
+    # extracting compositional matrix
+    A = np.dot(np.dot(Y[:n2], X[:n1].T).T, np.dot(Y[:n2],X[:n1].T))
+    B = np.dot(np.dot(Y[:n2].T, Y[:n2]), X[:n1].T)
+
+    return sv_trajectory_plots, predicted_sys_norm, predicted_non_sys_norm, quad_norms, U_preds, V_T_preds, sv_inidces
+
+# TODO: Questions to ask
+# TODO: Why are we specifying number of singular values to return if we have a function we use to return lower rank of the matrix
+# TODO: 
 
 if __name__ =='__main__':
   
